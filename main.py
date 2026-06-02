22aab401b10550e6ff82b155e8b36eb38323389e
Luis F. Rosales <Lfssjquantum@murena.io>
2026-06-01 23:37:07 -0400

    The project HoneyDipper has been started check # 10

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from manim import *
 from manim_slides import Slide
 
-config.tex_template.add_to_preamble(r"\usepackage{esint}")
+latex_aditional = TexTemplate()
+latex_aditional.add_to_preamble(r"\usepackage{esint}")
+Tex.set_default(tex_template = latex_aditional)
+MathTex.set_default(tex_template = latex_aditional)
 
 class title_page(Slide):
     def construct(self):
         logo = ImageMobject('honey.png').scale(0.4)
-        
         title = Text("Project HoneyDipper", color = '#F29C1F').scale(1.5)
-       # title.set_color_by_gradient(RED, YELLOW, GREEN, BLUE)
+        
+        t_and_logo = Group(logo, title)
+        t_and_logo.arrange(RIGHT, buff = 0.5)
+        t_and_logo.to_edge(UP, buff = 0.5)
 
-        t_and_logo = Group(logo, title).arrange(RIGHT, buff=0.5)
+        ##################### MAXWELL EQUATIONS #####################
+        gaussLaw = MathTex(r"\vec{\nabla} \cdot \vec{E} = \frac{\rho}{\varepsilon_0}")
+        gaussLaw_Magnetic = MathTex(r"\vec{\nabla} \cdot \vec{B} = 0")
+        faradayLaw = MathTex(r"\vec{\nabla} \times \vec{E} = - \frac{\partial \vec{B}}{\partial t}")
+        ampereLaw_MLaw = MathTex(r"\vec{\nabla} \times \vec{B} = \mu_0 \vec{J} + \mu_0 \varepsilon_0 \frac{\partial \vec{E}}{\partial t}")
 
-        t_and_logo.to_edge(UP, buff=0.5)
+        maxWelleq = VGroup(gaussLaw, gaussLaw_Magnetic, faradayLaw, ampereLaw_MLaw).scale(0.5)
+        maxWelleq.arrange(DOWN, aligned_edge = LEFT, buff = 0.5).to_corner(UP + LEFT, buff = 0.5)
+
+        title_eq = Tex(r"Maxwell's Equations").to_edge(UP, buff = 0.5)
+        #############################################################
 
         self.play(FadeIn(logo)) 
-       # self.next_slide() # Pause
-        
-        self.play(Write(title)) 
+        self.play(Write(title))
+
+        self.next_slide()
+
+        self.play(FadeOut(logo), Transform(title, title_eq))
+        self.play(Write(maxWelleq, run_time = 4))
